cd27045c7574558fa34ba61a5a45ef9d3182742a
Razan Srour <razanmsrour@gmail.com>
2025-01-19 00:10:11 +0300

    print the argv values

--- client.c
+++ client.c
@@ -6,15 +6,25 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:11:42 by rsrour            #+#    #+#             */
-/*   Updated: 2025/01/16 15:32:51 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/01/18 01:22:11 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 // ./client PID "message"
+/*
+ * CLient Checklist:
+ * - Wrtie a program (main) in which the client takes two parameters/arguments
+ *      - The PID of the server to which it wants to send the message
+ *      - A message
+ * - Encrypt the message (I did the encryption via bits)
+ * - Send the message to the server (via its PID)
+ * - Create a stop condition so that the server knows when it has finished 
+ *      receiving the message
+*/
 int main(int argc, char **argv)
 {
-    
+    printf("argc: %d, argv[0]: %s", argc, argv[0]);
     return (0);
 }
