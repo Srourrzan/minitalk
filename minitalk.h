461707c7ef46e98ca59f883dca91e0ce9aec6284
Razan Srour <razanmsrour@gmail.com>
2025-01-19 20:45:21 +0300

    create t_data type

--- minitalk.h
+++ minitalk.h
@@ -6,21 +6,25 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:01:14 by rsrour            #+#    #+#             */
-/*   Updated: 2025/01/19 19:31:02 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/01/19 20:41:39 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+# include "ft_printf.h"
+# include "libft.h"
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <sys/types.h>
-# include "libft.h"
-# include "ft_printf.h"
+# include <unistd.h>
 
-void    ft_signal(int, void *, int);
+typedef struct s_data
+{
+	int		s_pid;
+	char	*message;
+}			t_data;
 
 #endif
