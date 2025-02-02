59942aa9f41468b623b6b7458754875421d0e160
Razan Srour <razanmsrour@gmail.com>
2025-02-02 19:42:51 +0300

    create signal handler for the client

--- client.c
+++ client.c
@@ -6,22 +6,44 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 17:45:31 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/02 19:07:22 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/02 19:41:23 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int     main(int argc, char **argv)
+static int	g_signal;
+
+void	ft_g_set(int signal)
 {
-    t_data      *t_args;
-    
-    t_args = ft_parse_args(argv, argc);
-    if (!t_args)
-    {
-        ft_putendl_fd("Invalid client arguments", 2);
-        exit(1);
-    }
-    printf("pid = %d, message = %s\n", t_args->pid, t_args->message);
-    return (0);
+	if (signal == SIGUSR1)
+		g_signal = 1;
+}
+
+void	ft_client_sig_handler(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = &ft_g_set;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		(write(1, "Error\n", 6));
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		(write(1, "Error\n", 6));
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	*t_args;
+
+	t_args = ft_parse_args(argv, argc);
+	if (!t_args)
+	{
+		ft_putendl_fd("Invalid client arguments", 2);
+		exit(1);
+	}
+	printf("pid = %d, message = %s\n", t_args->pid, t_args->message);
+	ft_client_sig_handler();
+	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,27 +6,29 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 21:17:10 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/02 19:09:06 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/02 19:41:10 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <signal.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
-#include "ft_printf.h"
-#include "libft.h"
+# include "ft_printf.h"
+# include "libft.h"
+# include <signal.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef struct  s_data
+typedef struct s_data
 {
-    int       pid;
-    char    *message;
-}               t_data;
+	int		pid;
+	char	*message;
+}			t_data;
 
-int     ft_pid_is_numeric(char *src);
-t_data  *ft_parse_args(char **argv, int argc);
+int			ft_pid_is_numeric(char *src);
+t_data		*ft_parse_args(char **argv, int argc);
+void		ft_client_sig_handler(void);
+void		ft_g_set(int signal);
 
 #endif
