1ebb87055244a673356b6bc53dfe38290711d397
Razan Srour <razanmsrour@gmail.com>
2025-02-03 21:03:39 +0300

    create ft_init function

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 21:17:10 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/03 12:17:23 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/03 21:01:51 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
 
 typedef struct s_data
 {
@@ -43,5 +47,6 @@ t_data		*ft_parse_args(char **argv, int argc);
 void		ft_client_sig_handler(void);
 void		ft_g_set(int signal);
 void        ft_server_sig_handler();
+t_message       *ft_init(int pid);
 
 #endif
--- server_utils.c
+++ server_utils.c
@@ -6,8 +6,31 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 19:43:46 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/02 19:44:00 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/03 21:01:35 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+t_message       *ft_init(int pid)
+{
+    t_message   *new;
+    
+    new = malloc(sizeof(t_message));
+    if (!new)
+        return (NULL);
+    new->message = malloc(BUFFER_SIZE);
+    if (!new->message)
+    {
+        free(new);
+        return (NULL);
+    }
+    new->index = 0;
+    new->buffer_size = BUFFER_SIZE;
+    new->pid = pid;
+    new->buff = 0;
+    new->next = NULL;
+    new->bit_count = 0;
+    new->active = 1;
+    return (new);
+}
