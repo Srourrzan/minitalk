f46e98ec64eb2b67132d410f971590197eed9bee
Razan Srour <razanmsrour@gmail.com>
2025-02-04 06:59:38 +0300

    create recieve message function

--- server.c
+++ server.c
@@ -6,11 +6,31 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 21:16:46 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/02 20:13:29 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/03 21:58:47 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void    receive_message(int signal, siginfo_t *siginfo, void *context)
+{
+    static t_message    *head;
+    t_message           *curr;
+
+    (void)context;
+    if (head == NULL)
+        head = ft_init(siginfo->si_pid);
+    curr = ft_search_or_create(&head, siginfo->si_pid);
+    if (!curr || curr->active)
+        return ;
+    if (signal == SIGUSR1)
+        curr->buff |= 1;
+    curr->bit_count++;
+    if (curr->bit_count == 8)
+    {
+        
+    }
+}
 
 void    ft_server_sig_handler()
 {
