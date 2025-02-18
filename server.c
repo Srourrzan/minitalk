d252ce141ce10a8a733e85e591147495245bf0ad
Razan Srour <razanmsrour@gmail.com>
2025-02-18 10:24:46 +0300

    fix server files norms

--- server.c
+++ server.c
@@ -6,91 +6,91 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 21:16:46 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/09 21:16:02 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/18 10:22:42 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    ft_process_message(t_message **head, t_message **curr, siginfo_t *siginfo)
+void	ft_process_message(t_message **head, t_message **curr,
+		siginfo_t *siginfo)
 {
-    if (!(*curr)->active)
-        return ;
-    if ((*curr)->index == (*curr)->buffer_size - 1)
-    {
-        (*curr)->message = ft_expand_message_space((*curr)->message, 
-                            (*curr)->buffer_size + BUFFER_SIZE, (*curr)->buffer_size );
-        if(!(*curr)->message)
-        {
-            ft_remove_message(head, siginfo->si_pid);
-            (*curr) = NULL;
-            return;
-        }
-        (*curr)->buffer_size = (*curr)->buffer_size + BUFFER_SIZE;
-    }
-    (*curr)->message[(*curr)->index] = (*curr)->buff;
-    (*curr)->index = (*curr)->index + 1;
-    if ((*curr)->buff == 0)
-    {
-        ft_printf("%s\n", (*curr)->message);
-        (*curr)->active = 0;
-        ft_remove_message(head, siginfo->si_pid);
-        (*curr) = NULL;
-        kill(siginfo->si_pid, SIGUSR1);
-        return ;
-    }
+	if (!(*curr)->active)
+		return ;
+	if ((*curr)->index == (*curr)->buffer_size - 1)
+	{
+		(*curr)->message = ft_expand_message_space((*curr)->message,
+				(*curr)->buffer_size + BUFFER_SIZE, (*curr)->buffer_size);
+		if (!(*curr)->message)
+		{
+			ft_remove_message(head, siginfo->si_pid);
+			(*curr) = NULL;
+			return ;
+		}
+		(*curr)->buffer_size = (*curr)->buffer_size + BUFFER_SIZE;
+	}
+	(*curr)->message[(*curr)->index] = (*curr)->buff;
+	(*curr)->index = (*curr)->index + 1;
+	if ((*curr)->buff == 0)
+	{
+		ft_printf("%s\n", (*curr)->message);
+		(*curr)->active = 0;
+		ft_remove_message(head, siginfo->si_pid);
+		(*curr) = NULL;
+		kill(siginfo->si_pid, SIGUSR1);
+		return ;
+	}
 }
 
-void    ft_receive_message(int signal, siginfo_t *siginfo, void *context)
+void	ft_receive_message(int signal, siginfo_t *siginfo, void *context)
 {
-    static t_message    *head;
-    t_message           *curr;
+	static t_message	*head;
+	t_message			*curr;
 
-    (void)context;
-    if (head == NULL)
-        head = ft_init(siginfo->si_pid);
-    curr = ft_search_or_create(&head, siginfo->si_pid);
-    if (curr == NULL || !curr->active)
-        return ;
-    if (signal == SIGUSR1)
-        curr->buff |= 1;
-    curr->bit_count++;
-    if (curr->bit_count == 8)
-    {
-        ft_process_message(&head, &curr, siginfo);
-        if (curr)
-        {
-            curr->bit_count = 0;
-            curr->buff = 0;
-        }
-    }
-    else
-        curr->buff = curr->buff << 1;
-    ft_printf("sending signal to the client\n");
-    kill(siginfo->si_pid, SIGUSR1);
+	(void)context;
+	if (head == NULL)
+		head = ft_init(siginfo->si_pid);
+	curr = ft_search_or_create(&head, siginfo->si_pid);
+	if (curr == NULL || !curr->active)
+		return ;
+	if (signal == SIGUSR1)
+		curr->buff |= 1;
+	curr->bit_count++;
+	if (curr->bit_count == 8)
+	{
+		ft_process_message(&head, &curr, siginfo);
+		if (curr)
+		{
+			curr->bit_count = 0;
+			curr->buff = 0;
+		}
+	}
+	else
+		curr->buff = curr->buff << 1;
+	kill(siginfo->si_pid, SIGUSR1);
 }
 
-void    ft_server_sig_handler()
+void	ft_server_sig_handler(void)
 {
-    struct sigaction    sa;
-    
-    sa.sa_sigaction = &ft_receive_message;
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-        ft_putendl_fd("Error", 2);
-    if (sigaction(SIGUSR2, &sa, NULL) == -1)
-        ft_putendl_fd("Error", 2);
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &ft_receive_message;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_putendl_fd("Error", 2);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_putendl_fd("Error", 2);
 }
 
-int     main()
+int	main(void)
 {
-    pid_t   pid;
+	pid_t	pid;
 
-    pid = getpid();
-    ft_printf("Server PID: %i\n", pid);
-    ft_server_sig_handler();
-    while (1)
-        pause();
-    return (0);
+	pid = getpid();
+	ft_printf("Server PID: %i\n", pid);
+	ft_server_sig_handler();
+	while (1)
+		pause();
+	return (0);
 }
--- server_utils.c
+++ server_utils.c
@@ -6,63 +6,64 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 19:43:46 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/09 21:05:18 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/18 10:23:20 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_message       *ft_init(int pid)
+t_message	*ft_init(int pid)
 {
-    t_message   *new;
-    
-    new = malloc(sizeof(t_message));
-    if (!new)
-        return (NULL);
-    new->message = malloc(BUFFER_SIZE);
-    if (!new->message)
-    {
-        free(new);
-        return (NULL);
-    }
-    ft_bzero(new->message, BUFFER_SIZE);
-    new->index = 0;
-    new->buffer_size = BUFFER_SIZE;
-    new->pid = pid;
-    new->buff = 0;
-    new->next = NULL;
-    new->bit_count = 0;
-    new->active = 1;
-    return (new);
+	t_message	*new;
+
+	new = malloc(sizeof(t_message));
+	if (!new)
+		return (NULL);
+	new->message = malloc(BUFFER_SIZE);
+	if (!new->message)
+	{
+		free(new);
+		return (NULL);
+	}
+	ft_bzero(new->message, BUFFER_SIZE);
+	new->index = 0;
+	new->buffer_size = BUFFER_SIZE;
+	new->pid = pid;
+	new->buff = 0;
+	new->next = NULL;
+	new->bit_count = 0;
+	new->active = 1;
+	return (new);
 }
 
-t_message       *ft_search_or_create(t_message  **head, int pid)
+t_message	*ft_search_or_create(t_message **head, int pid)
 {
-    t_message       *current;
-    t_message       *new;
+	t_message	*current;
+	t_message	*new;
 
-    if (!*head)
-    {
-        *head = ft_init(pid);
-        return (*head);
-    }
-    current = *head;
-    while (current)
-    {
-        if (current->pid == pid)
-            return (current);
-        if (!current->next)
-        {
-            new = ft_init(pid);
-            current->next = new;
-            return (new);
-        }
-        current = current->next;
-    }
-    return (NULL);
+	if (!*head)
+	{
+		*head = ft_init(pid);
+		return (*head);
+	}
+	current = *head;
+	while (current)
+	{
+		if (current->pid == pid)
+			return (current);
+		if (!current->next)
+		{
+			new = ft_init(pid);
+			current->next = new;
+			return (new);
+		}
+		current = current->next;
+	}
+	return (NULL);
 }
 
-static void	*razan_memcpy(void *dst, const void *src, size_t size, size_t old_size)
+static void	*ftr_memcpy(void *dst, const void *src, size_t size,
+		size_t old_size)
 {
 	unsigned char	*s;
 	unsigned char	*d;
@@ -81,43 +82,43 @@ static void	*razan_memcpy(void *dst, const void *src, size_t size, size_t old_si
 	return (dst);
 }
 
-
-void    *ft_expand_message_space(void *message, size_t size, size_t old_size)//, size_t old_size)
+void	*ft_expand_message_space(void *message, size_t size, size_t old_size)
+		//, size_t old_size)
 {
-    void    *new_ptr;
+	void	*new_ptr;
 
-    new_ptr = malloc(size);
-    if (!new_ptr)
-        return (NULL);
-    ft_bzero(new_ptr, size);
-    new_ptr = razan_memcpy(new_ptr, message, size, old_size);
-    free(message);
-    return(new_ptr);
+	new_ptr = malloc(size);
+	if (!new_ptr)
+		return (NULL);
+	ft_bzero(new_ptr, size);
+	new_ptr = ftr_memcpy(new_ptr, message, size, old_size);
+	free(message);
+	return (new_ptr);
 }
 
-void    *ft_remove_message(t_message **head, int pid)
+void	*ft_remove_message(t_message **head, int pid)
 {
-    t_message       *curr;
-    t_message       *prev;
+	t_message	*curr;
+	t_message	*prev;
 
-    if (!head || !*head)
-        return (NULL);
-    curr = *head;
-    prev = NULL;
-    while (curr)
-    {
-        if (curr->pid == pid)
-        {
-            if (prev)
-                prev->next = curr->next;
-            else
-                *head = curr->next;
-            free(curr->message);
-            free(curr);
-            return (NULL);
-        }
-        prev = curr;
-        curr = curr->next;
-    }
-    return (NULL);
+	if (!head || !*head)
+		return (NULL);
+	curr = *head;
+	prev = NULL;
+	while (curr)
+	{
+		if (curr->pid == pid)
+		{
+			if (prev)
+				prev->next = curr->next;
+			else
+				*head = curr->next;
+			free(curr->message);
+			free(curr);
+			return (NULL);
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+	return (NULL);
 }
